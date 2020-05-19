ed4bcf6498a8051fc0d605a37da7c337c2715f91
shilu <shilulinc@126.com>
2020-05-19 23:12:42 +0800

    更新内容

--- Python练习/第三模块练习/类的定义.py
+++ Python练习/第三模块练习/类的定义.py
@@ -13,7 +13,11 @@ class Dog(object):
     def eat(self,food):
         print("%s is eating %s" %(self.NAME, food))
 
-d=Dog("lichuang")
+print(Dog)
+
+d=Dog("lichuang") # Dog(d, "Lichuang") # 实例化后产生的对象叫实例
 d2=Dog("chuang2")
 d.sayhi()
 d2.sayhi()
+
+d.eat("baozi")
